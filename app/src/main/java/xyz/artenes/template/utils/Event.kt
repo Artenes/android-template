7d17fbc0c4dbb61990f8597afead0da7756584ce
Artenes Nogueira <artenesama@gmail.com>
2024-04-20 18:04:19 -0400

    Adds ValueWithError util class

--- app/src/main/java/xyz/artenes/template/utils/Event.kt
+++ app/src/main/java/xyz/artenes/template/utils/Event.kt
@@ -1,5 +1,9 @@
 package xyz.artenes.template.utils
 
+/**
+ * This one is useful to send events from the ViewModel to the View
+ * such as when the view needs to be finished or to display a snack bar
+ */
 class Event(private val name: String? = null) {
 
     private var consumed: Boolean = false
---
+++ app/src/main/java/xyz/artenes/template/utils/ValueWithError.kt
@@ -0,0 +1,20 @@
+package xyz.artenes.template.utils
+
+/**
+ * This is useful for when you want to expose a value from your ViewModel
+ * that can have an error state as such:
+ *
+ *     private val _description = MutableStateFlow(ValueWithError())
+ *     val description: StateFlow<ValueWithError> = _description
+ *
+ * With this you have just one member variable in the ViewModel instead of two
+ * to represent the value and the error message
+ */
+data class ValueWithError(
+    val value: String = "",
+    val error: String? = null
+) {
+
+    fun hasError() = error != null
+
+}
