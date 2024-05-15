2412e849562ebcfc206942386ab2356b891ae312
Artenes Nogueira <artenesama@gmail.com>
2024-05-14 21:18:21 -0400

    Adds FormattedValue class

---
+++ app/src/main/java/xyz/artenes/template/core/FormattedValue.kt
@@ -0,0 +1,11 @@
+package xyz.artenes.template.core
+
+/**
+ * This is useful for when you need to pass down to the view
+ * both a value and its string representation to be displayed
+ * to the user.
+ */
+data class FormattedValue<T>(
+    val original: T,
+    val formatted: String
+)
